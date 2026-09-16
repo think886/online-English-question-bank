3cbcf51698f58265fdeb3914966933b8b9e3f4a7
LL <3440445985@qq.com>
2026-09-16 20:57:38 +0800

    框架简单构建

---
+++ English-question-bank/src/main/java/org/example/englishquestionbank/EnglishQuestionBankApplication.java
@@ -0,0 +1,13 @@
+package org.example.englishquestionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishQuestionBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishQuestionBankApplication.class, args);
+    }
+
+}
---
+++ English-question-bank/src/test/java/org/example/englishquestionbank/EnglishQuestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.englishquestionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishQuestionBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
